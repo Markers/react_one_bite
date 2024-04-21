694ea8b9258e5f255ec38c0173c6250b81a77491
markers <wizard870@gmail.com>
2024-04-22 06:51:26 +0900

    day 16 강의 완료

---
+++ section10/src/components/TodoItem.jsx
@@ -0,0 +1,43 @@
+import "./TodoItem.css";
+import { memo } from "react";
+
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckBox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeletButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        readOnly
+        onChange={onChangeCheckBox}
+        type="checkbox"
+        checked={isDone}
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toDateString()}</div>
+      <button onClick={onClickDeletButton}>삭제</button>
+    </div>
+  );
+};
+
+/*
+// HOC 고차 컴포넌트
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+  // True -> Props 가 바뀌지 않았다. -> 리렌더링 x
+  // False -> Props 가 바뀌었다. -> 리렌더링 o
+
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
+*/
+export default memo(TodoItem);
