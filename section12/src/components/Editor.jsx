5aea3b3a09a883a4dcd74ea84e793b75d02c4033
markers <wizard870@gmail.com>
2024-04-30 00:14:14 +0900

    day 21 강의 완료

---
+++ section12/src/components/Editor.jsx
@@ -0,0 +1,131 @@
+import Button from "./Button";
+import "./Editor.css";
+import "./EmotionItem";
+import EmotionItem from "./EmotionItem";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const emotionItemList = [
+  {
+    emotionId: 1,
+    emotionName: "완전좋음",
+  },
+  {
+    emotionId: 2,
+    emotionName: "좋음",
+  },
+  {
+    emotionId: 3,
+    emotionName: "보통",
+  },
+  {
+    emotionId: 4,
+    emotionName: "나쁨",
+  },
+  {
+    emotionId: 5,
+    emotionName: "완전나쁨",
+  },
+];
+
+const getStringDate = (targetDate) => {
+  // 날짜 -> YYYY-MM-DD
+  console.log("text", targetDate);
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let day = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  return `${year}-${month}-${day}`;
+};
+
+const Editor = ({ onSubmit }) => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const nav = useNavigate();
+
+  const onChangeInput = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
+
+  return (
+    <div className="Editor">
+      <section className="date_section">
+        <h4>오늘의 날짜</h4>
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringDate(input.createdDate)}
+          type="date"
+        />
+      </section>
+      <section className="emotion_section">
+        <h4>오늘의 감정</h4>
+        <div className="emotion_list_wrapper">
+          {emotionItemList.map((item) => (
+            <EmotionItem
+              onClick={() => {
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                });
+              }}
+              key={item.emotionId}
+              {...item}
+              isSelected={input.emotionId == item.emotionId}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+          placeholder="오늘은 어땠나요?"
+        />
+      </section>
+      <section className="button_section">
+        <Button onClick={() => nav(-1)} text={"취소하기"} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={"작성완료"}
+          type={"POSITIVE"}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Editor;
